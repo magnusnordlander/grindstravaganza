7a03b92b47d5845d506819e63662192b019e1f12
Magnus Nordlander <magnus@nordlander.se>
2021-03-27 23:01:04 +0100

    Added very simple report controller

---
+++ src/Controller/ReportController.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReportController extends AbstractController
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * @Route("/report", name="take_report")
+     */
+    public function takeReport(Request $request)
+    {
+        $this->logger->error($request->getContent());
+
+        return new Response("", Response::HTTP_CREATED);
+    }
+}
